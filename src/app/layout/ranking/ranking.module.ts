ae06679642535d8d0f931a3ef2906d3c818330c7
Luka Najjar <lukanajjar@hotmail.com>
2021-12-20 15:03:54 +0100

    Implements order by and filter in ranking page

--- src/app/layout/ranking/ranking.module.ts
+++ src/app/layout/ranking/ranking.module.ts
@@ -6,6 +6,8 @@ import { IonicModule } from '@ionic/angular';
 
 import { RankingPageRoutingModule } from './ranking-routing.module';
 import { LayoutComponentsModule } from '../components/layout-components/layout-components.module';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { OrderModule } from 'ngx-order-pipe';
 
 import { RankingPage } from './ranking.page';
 
@@ -16,6 +18,8 @@ import { RankingPage } from './ranking.page';
     IonicModule,
     RankingPageRoutingModule,
     LayoutComponentsModule,
+    Ng2SearchPipeModule,
+    OrderModule,
   ],
   declarations: [RankingPage],
 })
--- src/app/layout/ranking/ranking.page.ts
+++ src/app/layout/ranking/ranking.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UsersService } from 'src/app/api/users.service';
 import { ViewDidEnter } from '@ionic/angular';
-import { User } from 'src/app/models/user';
+import { UserRank } from 'src/app/models/user-rank';
+import { OrderPipe } from 'ngx-order-pipe';
 
 @Component({
   selector: 'app-ranking',
@@ -9,16 +10,31 @@ import { User } from 'src/app/models/user';
   styleUrls: ['./ranking.page.scss'],
 })
 export class RankingPage implements OnInit {
-  usersToDisplay: User[];
-  testToDisplay: string[] = ['test1', 'test2', 'test3'];
+  usersToDisplay: UserRank[];
+  searchTerm: string;
+  sortType: string;
+  sortReverse: boolean;
 
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private orderPipe: OrderPipe
+  ) {
+    this.sortType = 'rank';
+    this.sortReverse = true;
+  }
 
   ngOnInit() {}
 
   ionViewDidEnter() {
     this.usersService.getUsers().subscribe((users) => {
-      this.usersToDisplay = users;
+      this.usersToDisplay = users.map((user, index) => {
+        return {
+          rank: index + 1,
+          name: user.name,
+          money: user.money,
+        };
+      });
+      console.log(this.usersToDisplay);
     });
   }
 }
